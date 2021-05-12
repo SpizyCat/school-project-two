8cabd805056d61835723ef69fa7871afb63914a3
SpizyCat <spizy.yt@gmail.com>
2021-05-12 23:24:31 +0200

    Fixed everything.

--- script.js
+++ script.js
@@ -1,37 +1,52 @@
-window.onscroll = function() {setSticky();};
-
+var hamburgerButton = document.querySelector("#hamburger-menu-icon");
 var navbar = document.querySelector("#navbar");
-var content = document.querySelector(".content")
-var sticky = navbar.offsetTop;
+var content = document.querySelector(".content");
 
-function setSticky() {
-    if (window.pageYOffset >= sticky) {
-        navbar.classList.add("sticky")
-        content.style.paddingTop = "50px";
+function showHamburgerMenu() {
+    navbar.classList.add("open");
+}
 
+function hideHamburgerMenu() {
+    navbar.classList.remove("open");
+}
+
+function toggleHamburgerMenu() {
+    if (navbar.classList.contains("open")) {
+        hideHamburgerMenu();
+    } else {
+        showHamburgerMenu();
+    }
+}
+
+hamburgerButton.addEventListener("click", toggleHamburgerMenu);
+
+function setScrolled() {
+    if (window.scrollY > 0) {
+        navbar.classList.add("sticky");
     } else {
         navbar.classList.remove("sticky");
-        content.style.paddingTop = "0px";
     }
-    
 }
 var images = document.querySelectorAll(".image");
-for(var i = 0; i < images.length; i++){
-    images[i].addEventListener("click", e => {
+for (var i = 0; i < images.length; i++) {
+    images[i].querySelector("img").addEventListener("click", (e) => {
         var lightbox = document.querySelector("#img_lightbox");
-        lightbox.style.display = 'grid';
+        lightbox.style.display = "grid";
 
-        var preview_p = document.querySelector("#preview_p")  
-        var parent = e.target.parentNode
+        var preview_p = document.querySelector("#preview_p");
+        var parent = e.target.parentNode;
         lightbox.childNodes[1].src = parent.childNodes[1].currentSrc;
         preview_p.textContent = parent.childNodes[3].textContent;
         console.log(e.target.parentNode);
-    })
+    });
 }
 
-
-
 var span = document.querySelector(".close");
-span.onclick = function() { 
-    img_lightbox.style.display = "none";
-  }
+
+if (span) {
+    span.addEventListener("click", () => {
+        img_lightbox.style.display = "none";
+    });
+}
+
+document.addEventListener("scroll", setScrolled, {passive: true});
